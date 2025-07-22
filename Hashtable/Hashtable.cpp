2bf1afee0942b2f0cae1b123af9a7872bda5e682
shihao-hub <2958017271@qq.com>
2025-07-23 00:16:08 +0800

    general init

--- Hashtable/Hashtable.cpp
+++
@@ -1,1 +0,0 @@
-#include "Hashtable.h"
--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 
-
-template<typename ...Ts>
-void println(Ts...args) {
-    ((std::cout << args), ...) << std::endl;
-}
-
-
 int main() {
-    println("Hello, World!");
-    return 0;
+    std::cout << "Hello World!" << std::endl;
 }
